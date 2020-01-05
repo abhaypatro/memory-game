cb9afe7b736ce29882d33c3e1d1a891e1d093440
Abhay Charan Patro <abhaycharanpatro@gmail.com>
2020-01-05 15:41:09 +0530

    added game logic

--- game.js
+++ game.js
@@ -1,17 +1,31 @@
 var userClickedPattern=[];
 var buttonColours =["red", "blue", "green", "yellow"];
 var gamepattern=[];
+var level=0;
 
+var index=0;
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   animatePress(userChosenColour);
   var audioElement=new Audio("sounds/"+userChosenColour+".mp3");
   audioElement.play();
+  if(index<level)
+  {checkAnswer(index);
+  index++;
+  if(index>=level)
+  {
+  setTimeout(nextSequence,500);
+  }
+}
+
+
 });
 
 function nextSequence()
-{
+{ level++;
+  index=0;
+  $("h1").text("Level "+level);
   var randomNumber=Math.floor(Math.random()*4);
   var randomChosenColour=buttonColours[randomNumber];
   gamepattern.push(randomChosenColour);
@@ -22,4 +36,18 @@ function nextSequence()
 function animatePress(currentColour){
   $("#"+currentColour).addClass("pressed");
   setTimeout(function(){$("#"+currentColour).removeClass("pressed");  }, 100);
+}
+var ctr=0;
+
+$(document).keypress(function(event){
+    if(ctr==0)
+    {
+      ctr++;
+      nextSequence();
+    }
+});
+
+function checkAnswer(index){
+  if(userClickedPattern[index]==gamepattern[index])
+  return true;
 }
