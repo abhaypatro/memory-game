5062a4cdccb4e99728a46df25626ceed32ec7914
Abhay Charan Patro <abhaycharanpatro@gmail.com>
2020-01-05 10:16:48 +0530

    game.js update 1

--- game.js
+++ game.js
@@ -1,6 +1,18 @@
+var userClickedPattern=[];
 var buttonColours =["red", "blue", "green", "yellow"];
+var gamepattern=[];
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+});
+
 function nextSequence()
 {
-  randomNumber=Math.floor(Math.random()*3)+1;
-  console.log(randomNumber);
+  var randomNumber=Math.floor(Math.random()*4);
+  var randomChosenColour=buttonColours[randomNumber];
+  gamepattern.push(randomChosenColour);
+  var audioElement=new Audio("sounds/"+randomChosenColour+".mp3");
+  audioElement.play();
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
 }
