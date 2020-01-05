fb9e4e31d8564a645c7a11654a0518d12d9460b6
Abhay Charan Patro <abhaycharanpatro@gmail.com>
2020-01-05 10:36:17 +0530

    added animations

--- game.js
+++ game.js
@@ -5,6 +5,9 @@ var gamepattern=[];
 $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
+  animatePress(userChosenColour);
+  var audioElement=new Audio("sounds/"+userChosenColour+".mp3");
+  audioElement.play();
 });
 
 function nextSequence()
@@ -15,4 +18,8 @@ function nextSequence()
   var audioElement=new Audio("sounds/"+randomChosenColour+".mp3");
   audioElement.play();
   $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+}
+function animatePress(currentColour){
+  $("#"+currentColour).addClass("pressed");
+  setTimeout(function(){$("#"+currentColour).removeClass("pressed");  }, 100);
 }
